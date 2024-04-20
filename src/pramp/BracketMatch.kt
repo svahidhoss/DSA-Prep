49c2276fc5e2b41e4aa8a475cdb42bcc5a7cc0ff
LAPTOP-K2FI2B21\vahid <vahid.hoss@outlook.com>
2024-04-20 10:55:39 -0700

    Updates BracketMatch.kt

--- src/pramp/BracketMatch.kt
+++ src/pramp/BracketMatch.kt
@@ -1,5 +1,20 @@
 package pramp
 
+fun bracketMatch2(text: String): Int {
+    var count = 0
+    var result = 0
+
+    text.forEach {
+        if (it == '(')
+            count++
+        else {
+            if (count > 0) count--  // Match the current closing parenthesis with an unmatched opening one
+            else result++  // Increment result for each unmatched closing parenthesis
+        }
+    }
+    return result + count
+}
+
 fun bracketMatch(text: String): Int {
     var count = 0
     var result = 0
