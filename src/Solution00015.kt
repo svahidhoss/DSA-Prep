ea462fb9a05a60579c8697f77048c8e673b53fc3
LAPTOP-K2FI2B21\vahid <vahid.hoss@outlook.com>
2024-09-19 18:01:11 -0400

    Adds an optimized solution for "3Sum" problem using 2 pointers.

--- src/Solution00015.kt
+++ src/Solution00015.kt
@@ -1,15 +1,10 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution00015 {
-    fun threeSum(nums: IntArray): List<List<Int>> {
+
+    /**
+     * Time complexity: O(n^2)
+     * Space complexity: O(n)
+     */
+    fun threeSum2(nums: IntArray): List<List<Int>> {
         val result = mutableSetOf<List<Int>>()
 
         // Add all the values to the map
@@ -34,32 +29,56 @@ class Solution00015 {
         return result.toList()
     }
 
-    fun twoSumOrdered(numbers: IntArray, target: Int): IntArray {
-        var p = 0
-        var q = numbers.size - 1
+    /**
+     * Time complexity: O(n^2)
+     * Space complexity: O(1)
+     */
+    fun threeSum(nums: IntArray): List<List<Int>> {
+        val result = mutableSetOf<List<Int>>()
+        nums.sort()
+
+        nums.forEachIndexed { i, num ->
+            // call two pointer based 2sum to find -num as target
+            result.addAll(twoSumTwoPointer(nums, -num, i))
+        }
+
+        return result.toList()
+    }
+
+    private fun twoSumTwoPointer(numbers: IntArray, target: Int, begIndex: Int): MutableList<List<Int>> {
+        var p = begIndex + 1
+        var q = numbers.lastIndex
+        val result = mutableListOf<List<Int>>()
 
         while (p < q) {
             val sum = numbers[p] + numbers[q]
             when {
-                sum == target -> return intArrayOf(p + 1, q + 1)
+                sum == target -> {
+                    result.add(listOf(numbers[begIndex], numbers[p], numbers[q]))
+                    while (p < q && numbers[p] == numbers[p + 1]) p++ // Skip duplicates
+                    while (p < q && numbers[q] == numbers[q - 1]) q-- // Skip duplicates
+                    p++
+                    q--
+                }
                 sum < target -> p++
                 else -> q--
             }
         }
 
-        throw IllegalStateException("Valid answer was not found")
+        return result
     }
 }
 
 fun main() {
     val sol = Solution00015()
     var nums = intArrayOf(-1, 0, 1, 2, -1, -4)
-
     println(sol.threeSum(nums))
     nums = intArrayOf(0, 1, 1)
     println(sol.threeSum(nums))
     nums = intArrayOf(0, 0, 0)
     println(sol.threeSum(nums))
     nums = intArrayOf(0, 0, 0, 0)
+    println(sol.threeSum(nums))
+    nums = intArrayOf(-2, 0, 1, 1, 2)
     println(sol.threeSum(nums))
 }
