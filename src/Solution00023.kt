316aa1e2ae5f06d91bad91c98f8e478b3e3c1a15
LAPTOP-K2FI2B21\vahid <vahid.hoss@outlook.com>
2024-09-22 01:36:05 -0400

    Adds a more optimized solution for "Merge k Sorted Lists" problem.

--- src/Solution00023.kt
+++ src/Solution00023.kt
@@ -10,6 +10,37 @@ import java.util.*
  * }
  */
 class Solution00023 {
+
+    /**
+     * main loop O(N log k)
+     * result list: * O(N)
+     */
+    fun mergeKLists(lists: Array<ListNode?>): ListNode? {
+        val dummy = ListNode(-1)
+
+        // create a min heap based on the int value of list node
+        val minHeap = PriorityQueue<ListNode>(compareBy { it.`val` })
+        // Add the head of each non-null list to the heap
+        lists.filterNotNull().forEach {
+            minHeap.offer(it)
+        }
+
+        var currNode = dummy
+
+
+        while (minHeap.isNotEmpty()) {
+            val newNode = minHeap.poll()
+            currNode.next = newNode
+            currNode = newNode
+
+            newNode.next?.let {
+                minHeap.offer(it)
+            }
+        }
+
+        return dummy.next
+    }
+
     /**
      * Time Complexity(where N is the total number of nodes across all lists):
      *
@@ -21,7 +52,7 @@ class Solution00023 {
      * O(N) for the heap, where N is the total number of nodes.
      *
      */
-    fun mergeKLists(lists: Array<ListNode?>): ListNode? {
+    fun mergeKLists2(lists: Array<ListNode?>): ListNode? {
         val result = ListNode(-1)
 
         // create a min heap
@@ -29,7 +60,7 @@ class Solution00023 {
         lists.forEach { listNode ->
             var node = listNode
             while (node != null) {
-                minHeap.add(node?.`val`)
+                minHeap.offer(node?.`val`)
                 node = node?.next
             }
         }
@@ -45,7 +76,7 @@ class Solution00023 {
     }
 }
 
-fun main(args: Array<String>) {
+fun main() {
     val sol = Solution00023()
     val lists = arrayOf(
         createLinkedList(listOf(1, 4, 5)),
