3329c75cb173ee1c7008397d05bbc5ef3e229492
LAPTOP-K2FI2B21\vahid <vahid.hoss@outlook.com>
2025-03-05 15:19:33 -0500

    Adds a solution for "Kth Smallest Element in a BST" problem

--- src/java_solutions/TreeNode.java
+++ src/java_solutions/TreeNode.java
@@ -5,7 +5,13 @@ public class TreeNode {
     TreeNode left;
     TreeNode right;
 
-    TreeNode(int x) {
-        val = x;
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
     }
 }
