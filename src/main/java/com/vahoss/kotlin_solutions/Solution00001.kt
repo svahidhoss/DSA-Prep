f57aa166444795daa31b57feef32317b0b14d152
LAPTOP-K2FI2B21\vahid <vahid.hoss@outlook.com>
2025-09-16 21:32:52 -0400

    Improves the code

--- src/main/java/com/vahoss/kotlin_solutions/Solution00001.kt
+++ src/main/java/com/vahoss/kotlin_solutions/Solution00001.kt
@@ -1,11 +1,13 @@
 package com.vahoss.kotlin_solutions
 
-import java.lang.IllegalArgumentException
 import java.lang.IllegalStateException
 import java.util.*
 
 class Solution00001 {
 
+    /**
+     *  One-pass Hash Table solution
+     */
     fun twoSum(nums: IntArray, target: Int): IntArray {
         val map = mutableMapOf<Int, Int>()
         nums.forEachIndexed { i, num ->
@@ -18,64 +20,51 @@ class Solution00001 {
         throw IllegalStateException("No valid answer was found!")
     }
 
-    fun twoSum3(nums: IntArray, target: Int): IntArray {
-        val map = mutableMapOf<Int, MutableList<Int>>()
+    /**
+     * Two-pass solution
+     * --> Handles duplicates naturally (map stores the later index)
+     */
+    fun twoSum1(nums: IntArray, target: Int): IntArray {
+        val map = mutableMapOf<Int, Int>()
+        // First pass: build the hash map
+        nums.forEachIndexed { i, num ->
+            map[num] = i
+        }
 
-        nums.forEachIndexed { i, v ->
-            map[v]?.add(i) ?: apply {
-                map[target - v]?.let {
-                    return intArrayOf(it.first(), i)
-                }
-                map[v] = mutableListOf(i)
+        nums.forEachIndexed { i, num ->
+            val complement = target - num
+            map[complement]?.let { j ->
+                if (j != i) return intArrayOf(i, j)
             }
         }
 
-        throw IllegalStateException("No valid answer was found..")
+        throw IllegalStateException("No valid answer was found!")
     }
-
-    fun twoSum2(nums: IntArray, target: Int): IntArray {
-        var index1: Int? = null
-        var index2: Int? = null
-
-        val map = mutableMapOf<Int, MutableList<Int>>()
-        for (i in nums.indices) {
-            map[nums[i]]?.add(i) ?: apply {
-                map[nums[i]] = mutableListOf(i)
-            }
-        }
-
-        for (i in nums.indices) {
-            val potIndex = map[target - nums[i]]
-            if (potIndex != null) {
-                index1 = i
-                if (potIndex.size > 1) {
-                    for (i2 in potIndex)
-                        if (i2 != index1) index2 = i2
-                } else {
-                    if (potIndex[0] != index1) index2 = potIndex[0]
-                }
-
-                return intArrayOf(index1, index2!!)
-            }
-        }
-
-        throw IllegalArgumentException("No solution found. Code has problem?")
-    }
-
 }
 
 fun main() {
     val sol = Solution00001()
     var nums = intArrayOf(2, 7, 11, 15)
-    println(Arrays.toString(sol.twoSum3(nums, 9)))
+    println(Arrays.toString(sol.twoSum(nums, 9)))
+    println(Arrays.toString(sol.twoSum1(nums, 9)))
+
     nums = intArrayOf(2, 3, 4)
-    println(Arrays.toString(sol.twoSum3(nums, 6)))
+    println(Arrays.toString(sol.twoSum(nums, 6)))
+    println(Arrays.toString(sol.twoSum1(nums, 6)))
+
     nums = intArrayOf(3, 3)
     println(Arrays.toString(sol.twoSum(nums, 6)))
+    println(Arrays.toString(sol.twoSum1(nums, 6)))
+
     nums = intArrayOf(3, 2, 4)
-    println(Arrays.toString(sol.twoSum3(nums, 6)))
+    println(Arrays.toString(sol.twoSum(nums, 6)))
+    println(Arrays.toString(sol.twoSum1(nums, 6)))
+
     nums = intArrayOf(-1, 0)
     println(Arrays.toString(sol.twoSum(nums, -1)))
+    println(Arrays.toString(sol.twoSum1(nums, -1)))
+
     nums = intArrayOf(5, 25, 75)
     println(sol.twoSum(nums, 100).contentToString())
+    println(sol.twoSum1(nums, 100).contentToString())
 }
